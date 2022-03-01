354039ced9d227e74868552d1f0089f214a40ba6
tashfiaislamistila <tashfia.cse@gmail.com>
2022-02-28 20:03:55 -0800

    edit3

--- js/app.js
+++ js/app.js
@@ -12,26 +12,26 @@ const searchMobile = () => {
 }
 const displaySearchResult = (phones) => {
     console.log(phones);
-    // const searchResult = document.getElementById('search-result');
-    // // console.log(searchResult);
-    // const first20Data = phones.slice(0, 20);
-    // console.log(first20Data);
-    //     phones.forEach(phone => {
-    //         console.log(phone);
-    //         const div = document.createElement('div');
-    //         div.classList.add('col');
-    //         div.innerHTML = `
-    //       <div class="card h-100">
-    //          <img src="${phone.image}" class="card-img-top" alt="...">
-    //          <div class="card-body">
-    //              <h5 class="card-title">Card title</h5>
-    //              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-    //                 additional content. This content is a little bit longer.</p>
-    //          </div>
-    //   </div>
-    //       `;
-    //         searchResult.appendChild(div);
-    //     })
+    const searchResult = document.getElementById('search-result');
+    // console.log(searchResult);
+    const first20Data = phones.slice(0, 20);
+    console.log(first20Data);
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+          <div class="card h-100">
+             <img src="${phone.image}" class="card-img-top" alt="...">
+             <div class="card-body">
+                 <h5 class="card-title">Card title</h5>
+                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+             </div>
+      </div>
+          `;
+        searchResult.appendChild(div);
+    })
 
 
 }
