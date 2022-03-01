d3662b57e74197ac38ff455d53d8be3ae51336bc
tashfiaislamistila <tashfia.cse@gmail.com>
2022-02-28 22:25:47 -0800

    mobiledetails

--- js/app.js
+++ js/app.js
@@ -8,30 +8,37 @@ const searchMobile = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySearchResult(data.data));
+        .then(searchdata => displaySearchResult(searchdata.data));
 }
-const displaySearchResult = (phones) => {
-    // console.log(phones);
+const displaySearchResult = phones => {
+    console.log(phones);
     const searchResult = document.getElementById('search-result');
     // console.log(searchResult);
     // const first20Data = phones.slice(0, 20);
     // console.log(first20Data);
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-          <div class="card h-100">
+          <div class="card h-100 mb-3 w-75 mx-auto">
              <img src="${phone.image}" class="card-img-top" alt="...">
              <div class="card-body">
                  <h5 class="card-title">${phone.brand}</h5>
                  <h5 class="card-title">${phone.phone_name}</h5>
                  <div class="d-grid gap-2 col-6 mx-auto">
-               <button onclick="mobileDetails('phone.slug')"class="btn btn-primary" type="button">Details</button>
+               <button onclick="mobileDetails('${phone.slug}')"class="btn btn-primary" type="button">Details</button>
                 </div>
                </div>
             </div>
                  `;
         searchResult.appendChild(div);
     })
+}
+const mobileDetails = (mobileId) => {
+    // console.log(mobileId);
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(detailsdata => console.log(detailsdata.mainFeatures[2]));
 }
